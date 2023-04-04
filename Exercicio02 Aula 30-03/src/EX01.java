2f025654cc4aa149b728486cbd10227304c18606
Daniel-Moretti <126727072+Daniel-Moretti@users.noreply.github.com>
2023-04-04 20:13:29 -0300

    Update EX01.java

--- Exercicio02 Aula 30-03/src/EX01.java
+++ Exercicio02 Aula 30-03/src/EX01.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 //Imprima na tela o seu nome e sobrenome  Nome do Cachorro 30 vezes.
 public class EX01 {
     public static void main(String[] args) {
-        int cont;
-        for (cont = 1; cont<31; cont++){
-            System.out.printf("%d - Daniel A. S. Moretti.\n", cont);
-            System.out.printf("%d - Nome do meu pet é Sansa.\n\n", cont);
+        Scanner teclado = new Scanner(System.in);
+        String nome, sobrenome, nomeCachorro;
+
+        System.out.print("Informe seu nome: ");
+        nome = teclado.next();
+
+        System.out.print("Informe seu sobrenome: ");
+        sobrenome = teclado.next();
+
+        System.out.print("Informe o nome do seu pet: ");
+        nomeCachorro = teclado.next();
+
+        for (int cont = 1; cont<31; cont++){
+            System.out.println(cont + " - Nome: " + nome + " Sobrenome: " + sobrenome + " Nome do pet: " + nomeCachorro);
+
         }
         System.out.println("...Fim do For...");
 
