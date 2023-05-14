a21e29c172f4837f2ce9b082b43e25fdba12e1ea
Daniel Moretti <mrmoretti10@gmail.com>
2023-05-14 00:40:14 -0300

    13-05

--- Viagem/src/Carro.java
+++ Viagem/src/Carro.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Carro {
+public class Carro {
+    /***********************************
+     * Declaração de atributos privados*
+     **********************************/
+        private int idCarro;
+        private String modelo;
+        private String marca;
+        private Integer quilometragem;
+
+    /****************************************************************
+     * Declaração dos métodos públicos de encapsulamento (get e set)*
+     ***************************************************************/
+        public int getIdCarro(){
+            return idCarro;
+        }
+
+        public void setIdCarro(int idCarro){
+            this.idCarro = idCarro;
+        }
+
+        public String getModelo() {
+            return modelo;
+        }
+
+        public void setModelo(String modelo) {
+            this.modelo = modelo;
+        }
+
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public Integer getQuilometragem() {
+            return quilometragem;
+        }
+
+        public void setQuilometragem(Integer quilometragem) {
+            this.quilometragem = quilometragem;
+        }
+
+    /******************************************************
+     * Declaração dos métodos públicos restantes da classe*
+     *****************************************************/
+        public void acelerar(Integer quilometragem){
+            this.quilometragem += quilometragem;
+        }
+
 }
--- Viagem/src/Viagem.java
+++ Viagem/src/Viagem.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class Viagem {
+import java.util.Scanner;
+
+public class Viagem {
+    public static void main(String[] args) {
+        /**********************************************
+         *  Criando os objetos a partir da classe Carro *
+         *********************************************/
+            Carro carro1 = new Carro();
+            carro1 = Viajar(new Carro(), 1);
+
+            Carro carro2 = new Carro();
+            carro2 = Viajar(new Carro(), 2);
+
+            Carro carro3 = new Carro();
+            carro3 = Viajar(new Carro(), 3);
+
+        /*************************************
+         *  Apresentar na tela os resultados *
+         ************************************/
+            ApresentarInformacoes(carro1);
+            ApresentarInformacoes(carro2);
+            ApresentarInformacoes(carro3);
+
+
+    }
+
+    /**************************************************************
+     * Método para criar o objeto carro e fazer ele andar(viajar) *
+     * @param carro                                               *
+     * @param idCarro                                             *
+     * @return                                                    *
+     *************************************************************/
+        public static Carro Viajar(Carro carro, int idCarro){
+            Scanner teclado = new Scanner(System.in);
+
+            carro.setIdCarro(idCarro);
+
+            System.out.println("**********************************************************");
+            System.out.println("Digite o modelo do carro " + idCarro);
+            carro.setModelo(teclado.nextLine());
+
+            System.out.println("Digite a marca do carro " + idCarro);
+            carro.setMarca(teclado.nextLine());
+
+            System.out.println("Digite a quilometragem inicial do carro " + idCarro);
+            carro.setQuilometragem(teclado.nextInt());
+
+            int i = 1;
+            while (i < 5){
+                System.out.println("Digite quanto o carro " + idCarro + " vai andar na volta " + i);
+                carro.acelerar(teclado.nextInt());
+                i++;
+            }
+
+            return carro;
+
+        }
+
+    /***********************************************
+     * Método para apresentar informações do carro *
+      *********************************************/
+        public static void ApresentarInformacoes(Carro carro){
+            System.out.println("******************************************************");
+            System.out.println("Apresentar informações do carro " + carro.getIdCarro());
+            System.out.println("Marca: " + carro.getMarca() + " Modelo: " + carro.getModelo() +
+                    " Nova quilometragem: " + carro.getQuilometragem());
+
+            
+
+        }
+
+
 }
